8b9e3e7c7de9823dbae562058fdbe80c06ca56c7
Difroy <157180783+Difroy@users.noreply.github.com>
2025-10-06 16:18:17 +0200

    update

--- 0610/pomeriggio/Esercizio/Persona.cs
+++ 0610/pomeriggio/Esercizio/Persona.cs
@@ -2,17 +2,33 @@ public class Persona
 {
 
     public string Nome;
+    public string Cognome;
+    public int AnnoNascita;
     public int Eta;
 
-    public Persona(String nome, int eta)
+
+    public Persona(string nome, string cognome, int annoNascita)
     {
         Nome = nome;
-        Eta = eta;
+        Cognome = cognome;
+        AnnoNascita = annoNascita;
     }
 
     public void Saluta()
     {
-        Console.WriteLine($"Ciao mi chiamo {Nome} e ho {Eta} eta");
+        Console.WriteLine($"Ciao mi chiamo {Nome}");
+    }
+
+    public void CalcoloEta(int AnnoNascita)
+    {
+        const int AnnoCorrente = 2025;
+        AnnoNascita = this.AnnoNascita;
+        Eta = (AnnoCorrente - AnnoNascita);
+  
+    }
+    public void Presentazione()
+    {
+        Console.WriteLine($"{Nome} {Cognome} è nato nel {AnnoNascita} e ha {Eta} anni");
     }
 
 
--- 0610/pomeriggio/Esercizio/Program.cs
+++ 0610/pomeriggio/Esercizio/Program.cs
@@ -2,13 +2,15 @@
 public class Program
 {
 
-   static void Main(String[] args)
+    static void Main(String[] args)
     {
 
-        Persona p1 = new Persona("Francesca", 11);
-        Persona p2 = new Persona("Froy", 7);
-        p1.Saluta();
-        p2.Saluta();
+        Persona p1 = new Persona("Francesca", "Ernestino", 2001);
+        p1.CalcoloEta(p1.AnnoNascita);
+        p1.Presentazione();
+        /* Persona p2 = new Persona("Froy", 7); */
+        /* p1.Saluta();
+        p2.Saluta(); */
 
 
 
