44ad7153a488f8aa3b303f2a5e4dec7552a857fb
Difroy <157180783+Difroy@users.noreply.github.com>
2025-10-06 16:35:58 +0200

    Update

---
+++ 0610/pomeriggio/esercizioOperazioni/Operazioni.cs
@@ -0,0 +1,27 @@
+public class Operazioni
+{
+
+    int a;
+    int b;
+    int risultato;
+
+
+
+    public int Somma(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Moltiplicazione(int a, int b)
+    {
+        return a * b;
+
+    }
+
+public void StampaRisultato(string operazione, int risultato)
+    {
+        Console.WriteLine($"Il risultato dell'operazione {operazione} è: {risultato}");
+    }
+
+
+}
---
+++ 0610/pomeriggio/esercizioOperazioni/Program.cs
@@ -0,0 +1,29 @@
+﻿public class Program
+{
+
+    static void Main(string[] args)
+    {
+
+
+        Operazioni op = new Operazioni();
+
+        Console.WriteLine("Inserisci il primo numero: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Inserisci il secondo numero: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        int somma = op.Somma(num1, num2);
+        op.StampaRisultato("Somma", somma);
+        int prodotto = op.Moltiplicazione(num1, num2);
+        op.StampaRisultato("Prodotto", prodotto);
+
+
+    }
+
+
+
+}
+
+
+
