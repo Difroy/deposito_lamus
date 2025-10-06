5f8c771521329ed8e11f82324e8694295f78e80a
Difroy <157180783+Difroy@users.noreply.github.com>
2025-10-06 11:54:28 +0200

    update
    manca sviluppo metodo di stampa del riepilogo ordine + registro, anche fase di testing

--- 0610/mattina/DolceGelo/DolceGelo.cs
+++ 0610/mattina/DolceGelo/DolceGelo.cs
@@ -20,22 +20,59 @@ class DolceGelo
         double[] prezzi = { 2.50, 3, 2, 4, 1.50 };
 
         double totale = 0;
-        int numeroPalline = 0;
+        int totalePalline = 0;
 
 
-      static void StampaMenu(string[] gusti, double[] prezzi)
-    {
-        Console.WriteLine("\n===== MENU GELATERIA =====");
-        for (int i = 0; i < gusti.Length; i++)
+        while (true)
         {
-            Console.WriteLine($"[{i}] {gusti[i]} - €{prezzi[i]:F2} a pallina");
+            StampaMenu(gusti, prezzi);
+
+            Console.Write("\nInserisci lìindice del gusto (0-4) o -1 per terminare: ");
+            int indiceGusto = int.Parse(Console.ReadLine());
+
+            if (indiceGusto == -1)
+            {
+                break;
+            }
+            if (indiceGusto < 0 || indiceGusto >= gusti.Length)
+            {
+                Console.WriteLine("Indice non valido. Riprova \n");
+                continue;
+            }
+            Console.Write("Inserisci la quantità di palline: ");
+            int quantita = int.Parse(Console.ReadLine());
+            if (quantita <= 0)
+            {
+                Console.WriteLine("Errore: La quantità deve essere positiva! Riprova.\n");
+                continue;
+            }
+
+            double prezzoGusto = CalcolaTotale(indiceGusto, quantita, prezzi);
+            totale += prezzoGusto;
+            totalePalline += quantita;
+
+            Console.WriteLine($"\nAggiunto: {quantita}x {gusti[indiceGusto]} = €{prezzoGusto:F2}\n");
+
         }
-    }   
+        
+    
+        static void StampaMenu(string[] gusti, double[] prezzi)
+        {
+            Console.WriteLine("\n===== MENU GELATERIA =====");
+            for (int i = 0; i < gusti.Length; i++)
+            {
+                Console.WriteLine($"[{i}] {gusti[i]} - €{prezzi[i]:F2} a pallina");
+            }
+        }
+
+        static double CalcolaTotale(int gusto, int quantita, double[] prezzi)
+        {
+            return prezzi[gusto] * quantita;
+        }
 
 
 
-
-}
+    }
 
 
 
