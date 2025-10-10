632b978fabc76c0838209bad48f80c5cc4d59ca4
Difroy <157180783+Difroy@users.noreply.github.com>
2025-10-10 15:13:01 +0200

    Add DispositivoElettronico class: define abstract methods and properties for electronic devices

---
+++ 1010/DispositivoElettronico/DispositivoElettronico.cs
@@ -0,0 +1,22 @@
+namespace DispositivoElettronico
+{
+    public abstract class DispositivoElettronico
+    {
+        public string Modello { get; set; }
+
+
+
+        public DispositivoElettronico(string modello)
+        {
+            Modello = modello;
+        }
+
+
+        public abstract void Accendi();
+        public abstract void Spegni();
+        public virtual void MostraInfo()
+        {
+            Console.WriteLine($"Modello: {Modello}");
+        }
+    }
+}
