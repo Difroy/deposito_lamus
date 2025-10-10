a3b4b7862292d4ab2b099e5d43dd3734d00c5192
Difroy <157180783+Difroy@users.noreply.github.com>
2025-10-10 11:52:20 +0200

    Refactor Prodotto class: improve property definitions and validation logic

--- 1010/GestioneBiblioteca/Prodotto.cs
+++ 1010/GestioneBiblioteca/Prodotto.cs
@@ -1,12 +1,60 @@
 namespace GestioneBiblioteca
 {
-    
-public abstract class Prodotto
+
+    public abstract class Prodotto
     {
 
         private string titolo;
         private string annoPubblicazione;
         private double prezzo;
+
+
+        public Prodotto(string titolo, int annoPubblicazione, double prezzo)
+        {
+            Titolo = titolo;
+            AnnoPublicazione = annoPubblicazione;
+            Prezzo = prezzo;
+        }
+        public string Titolo
+        {
+            get { return titolo; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Il titolo non può essere vuoto.");
+                    titolo = value;
+                }
+
+            }
+        }
+
+        public int AnnoPublicazione
+        {
+            get { return int.Parse(annoPubblicazione); }
+            set
+            {
+                if (value < 1000 || value > DateTime.Now.Year)
+                {
+                    throw new ArgumentException("L'anno di pubblicazione deve essere un numero positivo.");
+                }
+                annoPubblicazione = value.ToString();
+            }
+        }
+
+        public double Prezzo
+        {
+            get { return prezzo; }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Il prezzo non può essere negativo");
+                prezzo = value;
+            }
+        }
+
+
+
 
     }
 
